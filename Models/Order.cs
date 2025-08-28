0828a9daafd0b2aebf2fc54f80574ce5e357b82b
Cindy GY <162321776+Wu-Cindy-GuanYing@users.noreply.github.com>
2025-08-28 22:51:51 +0200

    sixth commit
    Committing Models

---
+++ Models/Order.cs
@@ -0,0 +1,65 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System.ComponentModel.DataAnnotations;
+
+namespace ABCRetailers.Models
+{
+    public class Order : ITableEntity
+    {
+        public string PartitionKey { get; set; } = "Order";
+
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+        public DateTimeOffset? Timestamp {  get; set; }
+        public ETag ETag { get; set; }
+
+        [Display(Name = "Order ID")]
+        public string OrderId => RowKey;
+
+        [Required]
+        [Display(Name = "Customer")]
+        public string CustomerId { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Product")]
+        public string ProductId { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Order Date")]
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; } = DateTime.SpecifyKind(DateTime.UtcNow.Date, DateTimeKind.Utc);
+
+        [Required]
+        [Display(Name = "Quantity")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Unit Price")]
+        [DataType(DataType.Currency)]
+        public double UnitPrice { get; set; }
+
+        [Display(Name = "Total Price")]
+        [DataType(DataType.Currency)]
+        public double TotalPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Status")]
+        [DataType(DataType.Currency)]
+        public string Status { get; set; } = "Submitted";
+    }
+
+    public enum OrderStatus
+    {
+        Submitted, //order is first created
+        Processing, //company opens/reviews the order
+        Completed, //order is delivered to customer
+        Cancelled //order is cancelled
+    }
+}
